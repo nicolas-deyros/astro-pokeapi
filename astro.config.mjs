cfaa9bffc8bbdd46f550052dbed5dbc17354b913
Nicolas Deyros <ndeyros@gmail.com>
2022-09-14 15:57:45 -0300

    Added Tailwind, React, Image and Icon. Rebuild the solution using this integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+
+import image from '@astrojs/image'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), react(), image()],
+	vite: {
+		ssr: {
+			external: ['svgo'],
+		},
+	},
+})
