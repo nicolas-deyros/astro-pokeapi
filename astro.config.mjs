37c59729c401e10c21f0ec04f76a0ba0255acc36
Nicolás Deyros <ndeyros@gmail.com>
2022-09-18 18:15:57 -0300

    Tailwind color config error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,29 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-
 import image from '@astrojs/image'
+import prefetch from '@astrojs/prefetch'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), image()],
+	integrations: [
+		tailwind({
+			config: {
+				applyBaseStyles: true,
+				applyAstroPreset: false,
+			},
+		}),
+		react(),
+		image(),
+		prefetch(),
+	],
 	vite: {
 		ssr: {
 			external: ['svgo'],
 		},
 	},
+	output: 'server',
+	adapter: vercel(),
 })
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+
+const FONT_FAMILY_BASE = [
+	'system-ui',
+	'-apple-system',
+	'BlinkMacSystemFont',
+	'Segoe UI',
+	'Roboto',
+	'Oxygen',
+	'Ubuntu',
+	'Cantarell',
+	'Open Sans',
+	'Helvetica Neue',
+	'sans-serif',
+]
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			body: FONT_FAMILY_BASE,
+			display: ['RT Alias Medium', ...FONT_FAMILY_BASE],
+			mono: [
+				'Menlo',
+				'Monaco',
+				'Lucida Console',
+				'Liberation Mono',
+				'DejaVu Sans Mono',
+				'Bitstream Vera Sans Mono',
+				'Courier New',
+				'monospace',
+			],
+		},
+		extend: {
+			colors: {
+				bug: '#aedf78',
+				water: '#43ccff',
+				grass: '#00ca91',
+				fire: '#e95c4d',
+				normal: '#a5a5a5',
+			},
+		},
 	},
 	plugins: [],
 }
