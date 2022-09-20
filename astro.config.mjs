9fd0c541c9d279a6cae7a34e38357348d7f6b8e2
Nicolas Deyros <ndeyros@gmail.com>
2022-09-20 18:15:11 -0300

    Add slug page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,26 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import image from '@astrojs/image'
-import prefetch from '@astrojs/prefetch'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
+import prefetch from '@astrojs/prefetch';
+import vercel from '@astrojs/vercel/serverless';
+import sitemap from "@astrojs/sitemap";
 
-import vercel from '@astrojs/vercel/serverless'
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: true,
-				applyAstroPreset: false,
-			},
-		}),
-		react(),
-		image(),
-		prefetch(),
-	],
-	vite: {
-		ssr: {
-			external: ['svgo'],
-		},
-	},
-	output: 'server',
-	adapter: vercel(),
-})
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: true,
+      applyAstroPreset: false
+    }
+  }), react(), image(), prefetch(), sitemap(), compress()],
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
+  output: 'server',
+  adapter: vercel()
+});
--- src/stores/loadMore.js
+++
@@ -1,9 +0,0 @@
-import { atom } from 'nanostores'
-
-const initialValue = { value: 15 }
-
-const loadMore = atom(initialValue)
-
-const increaseLoadMore = () => loadMore.set({ value: loadMore.get().value + 5 })
-
-export { loadMore, increaseLoadMore }
